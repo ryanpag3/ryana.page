e5ecb0cb0740979ecfbd0ecb69b5977a31ca35dc
Ryan Page <ryanapage@gmail.com>
2024-11-10 14:35:53 -0800

    setup react-router (just in case)

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomeScreen from './screens/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen/>,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
