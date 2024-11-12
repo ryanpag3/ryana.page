ec42366ae87c7ae34ed7c034c4785b5e9140a181
Ryan Page <ryanapage@gmail.com>
2024-11-12 08:48:04 -0800

    remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Colors from './const/colors';
-import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/Home';
 import GlobalStyle from './styles/GlobalStyle';
 
@@ -26,8 +25,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
